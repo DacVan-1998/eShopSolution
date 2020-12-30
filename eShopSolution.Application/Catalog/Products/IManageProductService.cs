ae485a95a8784963b38ca3b4a33834dbf62b5b6b
Dac Van <van264661@nuce.edu.vn>
2020-12-30 23:11:15 +0700

    Add paging method

--- eShopSolution.Application/Catalog/Products/IManageProductService.cs
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -1,4 +1,5 @@
 ﻿using eShopSolution.Application.Catalog.Products.Dtos;
+using eShopSolution.Application.Catalog.Products.Dtos.Manage;
 using eShopSolution.Application.Dtos;
 using System;
 using System.Collections.Generic;
@@ -7,14 +8,17 @@ using System.Threading.Tasks;
 
 namespace eShopSolution.Application.Catalog.Products
 {
-   public interface IManageProductService
+    public interface IManageProductService
     {
         Task<int> Create(ProductCreateRequest request);
-        Task<int> Update(ProductEditRequest request);
+        Task<int> Update(ProductUpdateRequest request);
         Task<int> Delete(int productId);
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+        Task<bool> UpdateStock(int productId, int addedQuantity);
 
+        Task AddViewCount(int productId);
         Task<List<ProductViewModel>> GetAll();
-        Task<PageViewModel<ProductViewModel>> GetAllPaging(string keyword,int pageIndex,int pageSize);
+        Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
 
     }
 }
---
+++ eShopSolution.Utilities/Exceptions/eShopException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Utilities.Exceptions
+{
+    public class eShopException : Exception
+    {
+        public eShopException() 
+        { 
+        }
+
+        public eShopException(string message) : base(message)
+        {
+        }
+
+        public eShopException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
